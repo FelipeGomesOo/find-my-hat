d8b891ccb10f64884def13ca6c1ebd3d34602b7c
Felipe Gomes <contato@felipe-gomes.com>
2024-04-04 19:22:23 -0300

    feat: Add gh-pages support and deploy to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
-import React from 'react';
-import './styles/styles.scss'; 
-import { GameProvider } from './gameContext';
-import Game from './components/Game';
-import Root from './components/Root';
-import {RouterProvider, createBrowserRouter, createRoutesFromElements, Route} from 'react-router-dom'; 
+import React from "react";
+import "./styles/styles.scss";
+import { GameProvider } from "./gameContext";
+import Game from "./components/Game";
+import Root from "./components/Root";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
 const appRouter = createBrowserRouter(
-    createRoutesFromElements(
-        <>
-        <Route path='/' element={<Root/>}>  
-            <Route index element={<Game /> } />  
-        </Route>
-        </>
-    ),
-    {
-        basename: '/findMyHat', 
-    }
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<Root />}>
+        <Route index element={<Game />} />
+      </Route>
+    </>
+  ),
+  {
+    basename: "/find-my-hat",
+  }
 );
 
 export default function App() {
   return (
     <div className="App">
       <GameProvider>
-        <RouterProvider router={appRouter}/>  
+        <RouterProvider router={appRouter} />
       </GameProvider>
     </div>
   );
-} 
+}
